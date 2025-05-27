10b3d5c41836504b3a208babf9e540db06f5989e
VistarakulaVijay <vijayvista2004@gmail.com>
2025-05-27 23:35:08 +0530

    university-middleware.js

---
+++ web-app/middleware/university-middleware.js
@@ -0,0 +1,25 @@
+const logger = require('../services/logger');
+
+
+
+
+function authenticateLogin (req, res, next) {
+    try {
+        if (req.session.user_type === "university") next();
+        else throw new Error("Unauthorized access: Login first");
+    } catch (e) {
+        next(e);
+    }
+}
+
+function redirectToDashboardIfLoggedIn(req,res,next) {
+    try {
+        if (req.session.user_type === "university") return res.redirect('/university/dashboard');
+        else next();
+    } catch (e) {
+        next(e);
+    }
+}
+
+
+module.exports = {authenticateLogin, redirectToDashboardIfLoggedIn};
